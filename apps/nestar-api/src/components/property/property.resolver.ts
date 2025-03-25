db792cb1b79600f81724de52538bbf5a033bd3e7
mirzohidd <mirzohidmurodov77@gmail.com>
2025-03-25 22:26:24 +1000

    feat:develop getProperties GraphQL API

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -1,16 +1,17 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 
 import { PropertyService } from './property.service';
-import { Property } from '../../libs/dto/property/property';
-import { PropertyInput } from '../../libs/dto/property/property.input';
+import { Properties, Property } from '../../libs/dto/property/property';
+import { AgentPropertiesInquiry, PropertiesInquiry, PropertyInput } from '../../libs/dto/property/property.input';
 import { MemberType } from '../../libs/enums/member.enum';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { UseGuards } from '@nestjs/common';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { AuthMember } from '../auth/decorators/authMember.decorator';
 import { ObjectId } from 'mongoose';
-import { shapeIntoMongoObjectId } from '../../libs/config';
 import { WithoutGuard } from '../auth/guards/without.guard';
+import { shapeIntoMongoObjectId } from '../../libs/config';
+import { PropertyUpdate } from '../../libs/dto/property/property.update';
 
 @Resolver()
 export class PropertyResolver {
@@ -30,27 +31,50 @@ export class PropertyResolver {
 	}
 
 	@UseGuards(WithoutGuard)
-	@Query((returns) => Property)
+	@Query(() => Property)
 	public async getProperty(
 		@Args('propertyId') input: string,
-		@AuthMember('_id') memberId: ObjectId,
+		@AuthMember('_id') memberId: ObjectId | null,
 	): Promise<Property> {
 		console.log('Query: getProperty');
-		const propertyId = shapeIntoMongoObjectId(input);
+		const propertyID = shapeIntoMongoObjectId(input);
 
-		return await this.propertyService.getProperty(memberId, propertyId);
+		return await this.propertyService.getProperty(memberId, propertyID);
 	}
 
 	@Roles(MemberType.AGENT)
- 	@UseGuards(RolesGuard)
- 	@Mutation((returns) => Property)
- 	public async updateProperty(
- 		@Args('input') input: PropertyUpdate,
- 		@AuthMember('_id') memberId: ObjectId,
- 	): Promise<Property> {
- 		console.log('Mutation: updateProperty');
- 		input._id = shapeIntoMongoObjectId(input._id);
- 
- 		return await this.propertyService.updateProperty(memberId, input);
- 	}
+	@UseGuards(RolesGuard)
+	@Mutation((returns) => Property)
+	public async updateProperty(
+		@Args('input') input: PropertyUpdate,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Property> {
+		console.log('Mutation: updateProperty');
+		input._id = shapeIntoMongoObjectId(input._id);
+
+		return await this.propertyService.updateProperty(memberId, input);
+	}
+
+	@UseGuards(WithoutGuard)
+	@Query(() => Properties)
+	public async getProperties(
+		@Args('input') input: PropertiesInquiry,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Properties> {
+		console.log('Query: getProperties');
+
+		return await this.propertyService.getProperties(memberId, input);
+	}
+
+	@Roles(MemberType.AGENT)
+	@UseGuards(RolesGuard)
+	@Query(() => Properties)
+	public async getAgentProperties(
+		@Args('input') input: AgentPropertiesInquiry,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Properties> {
+		console.log('Query: getAgentProperties');
+
+		return await this.propertyService.getAgentProperties(memberId, input);
+	}
 }
--- apps/nestar-api/src/components/property/property.service.ts
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -1,24 +1,29 @@
 import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, ObjectId } from 'mongoose';
-import { AuthService } from '../auth/auth.service';
 import { ViewService } from '../view/view.service';
-import { PropertyInput } from '../../libs/dto/property/property.input';
-import { Property } from '../../libs/dto/property/property';
-import { Message } from '../../libs/enums/common.enum';
+import {
+	AgentPropertiesInquiry,
+	PISearch,
+	PropertiesInquiry,
+	PropertyInput,
+} from '../../libs/dto/property/property.input';
+import { Properties, Property } from '../../libs/dto/property/property';
+import { Direction, Message } from '../../libs/enums/common.enum';
 import { MemberService } from '../member/member.service';
-import { ViewInput } from '../../libs/dto/view/view.input';
 import { PropertyStatus } from '../../libs/enums/property.enum';
-import { ViewGroup } from '../../libs/enums/view.enum';
 import { StatisticModifier, T } from '../../libs/types/common';
+import { ViewInput } from '../../libs/dto/view/view.input';
+import { ViewGroup } from '../../libs/enums/view.enum';
 import { PropertyUpdate } from '../../libs/dto/property/property.update';
+import moment from 'moment';
+import { lookupMember, shapeIntoMongoObjectId } from '../../libs/config';
 
 @Injectable()
 export class PropertyService {
 	constructor(
 		@InjectModel('Property') private readonly propertyModel: Model<Property>,
 		private memberService: MemberService,
-		private authService: AuthService,
 		private viewService: ViewService,
 	) {}
 
@@ -30,11 +35,11 @@ export class PropertyService {
 			return result;
 		} catch (err) {
 			console.log('Error: Service.model:', err.message);
-			throw new BadRequestException(Message.USED_MEMBER_NICK_OR_PHONE);
+			throw new BadRequestException(Message.CREATE_FAILED);
 		}
 	}
 
-	public async getProperty(memberId: ObjectId, propertyId: ObjectId): Promise<Property> {
+	public async getProperty(memberId: ObjectId | null, propertyId: ObjectId): Promise<Property> {
 		const search: T = {
 			_id: propertyId,
 			propertyStatus: PropertyStatus.ACTIVE,
@@ -58,26 +63,33 @@ export class PropertyService {
 		targetProperty.memberData = await this.memberService.getMember(null, targetProperty.memberId);
 		return targetProperty;
 	}
+
 	public async propertyStatsEditor(input: StatisticModifier): Promise<Property> {
 		const { _id, targetKey, modifier } = input;
 		return (await this.propertyModel
 			.findByIdAndUpdate({ _id }, { $inc: { [targetKey]: modifier } }, { new: true })
 			.exec()) as unknown as Property;
 	}
+
 	public async updateProperty(memberId: ObjectId, input: PropertyUpdate): Promise<Property> {
-	
+		// 1. We didnt update the soldAt and deletedAt fields in the database. We need to update them
+		// 2. MongoDB error: dupplicate key error
+		// 3. "propertyStatus": "SOLD"?  INTERNAL_SERVER_ERROR: "message": "(0 , moment_1.default) is not a function"
 		// moment  considered legacy. Use other alternatives
 		let { propertyStatus, soldAt, deletedAt } = input;
-		
-		
+		console.log('propertyStatus:', propertyStatus);
+		console.log('soldAt:', soldAt);
+		console.log('deletedAt:', deletedAt);
+
 		const search: T = {
 			_id: input._id,
 			memberId: memberId,
 			propertyStatus: PropertyStatus.ACTIVE,
 		};
 
-		
-		
+		// if (propertyStatus === PropertyStatus.SOLD) soldAt = moment().toDate();
+		// else if (propertyStatus === PropertyStatus.DELETE) deletedAt = moment().toDate();
+
 		if (propertyStatus === PropertyStatus.SOLD) soldAt = new Date();
 		else if (propertyStatus === PropertyStatus.DELETE) deletedAt = new Date();
 
@@ -89,5 +101,99 @@ export class PropertyService {
 		}
 
 		return result;
-	
+	}
+
+	public async getProperties(memberId: ObjectId, input: PropertiesInquiry): Promise<Properties> {
+		const { page, limit, sort, direction, search } = input;
+
+		const match: T = { propertyStatus: PropertyStatus.ACTIVE };
+		const sortFinal: T = { [sort ?? 'createdAt']: direction ?? Direction.DESC };
+
+		this.shapeMatchQuery(match, search);
+		console.log('match:', match);
+
+		const result = await this.propertyModel
+			.aggregate([
+				{ $match: match },
+				{ $sort: sortFinal },
+				{
+					$facet: {
+						list: [
+							{ $skip: page - 1 },
+							{ $limit: limit },
+							//meliked
+							lookupMember,
+							{ $unwind: '$memberData' },
+						],
+
+						metaCounter: [{ $count: 'total' }],
+					},
+				},
+			])
+			.exec();
+		if (!result) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+		return result[0];
+	}
+
+	private shapeMatchQuery(match: T, search: PISearch): void {
+		const {
+			memberId,
+			locationList,
+			typeList,
+			roomList,
+			bedsList,
+			options,
+			pricesRange,
+			periodsRange,
+			squaresRange,
+			text,
+		} = search;
+
+		if (memberId) match.memberId = shapeIntoMongoObjectId(memberId);
+		if (locationList) match.propertyLocation = { $in: locationList };
+		if (roomList) match.propertyRooms = { $in: roomList };
+		if (bedsList) match.propertyBeds = { $in: bedsList };
+		if (typeList) match.propertyType = { $in: typeList };
+
+		if (pricesRange) match.propertyPrice = { $gte: pricesRange.start, $lte: pricesRange.end };
+		if (periodsRange) match.constructedAt = { $gte: periodsRange.start, $lte: periodsRange.end };
+		if (squaresRange) match.propertySquare = { $gte: squaresRange.start, $lte: squaresRange.end };
+
+		if (text) match.propertyTitle = { $regex: text, $options: 'i' };
+		if (options) {
+			match['$or'] = options.map((ele) => {
+				return { [ele]: true };
+			});
+		}
+	}
+
+	public async getAgentProperties(memberId: ObjectId, input: AgentPropertiesInquiry): Promise<Properties> {
+		const { page, limit, sort, direction, search } = input;
+
+		const { propertyStatus } = search;
+		if (propertyStatus === PropertyStatus.DELETE) throw new BadRequestException(Message.NOT_ALLOWED_REQUEST);
+
+		const match: T = {
+			memberId: memberId,
+			propertyStatus: propertyStatus ?? { $ne: PropertyStatus.DELETE },
+		};
+		const sortFinal = { [sort ?? 'createdAt']: direction ?? Direction.DESC };
+
+		const result = await this.propertyModel
+			.aggregate([
+				{ $match: match },
+				{ $sort: sortFinal },
+				{
+					$facet: {
+						list: [{ $skip: page - 1 }, { $limit: limit }, lookupMember, { $unwind: '$memberData' }],
+						metaCounter: [{ $count: 'total' }],
+					},
+				},
+			])
+			.exec();
+		if (!result) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+		return result[0];
+	}
 }
