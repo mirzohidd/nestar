eed3ca74dbaa09c4e22c937583000bf296bf1c28
mirzohidd <mirzohidmurodov77@gmail.com>
2025-04-08 21:50:50 +1000

    feat: create batchRollback, batchProperties, batchAgents job schedulers logic

--- apps/nestar-batch/src/batch.controller.ts
+++ apps/nestar-batch/src/batch.controller.ts
@@ -1,12 +1,66 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { BatchService } from './batch.service';
+import { Cron, Interval, Timeout } from '@nestjs/schedule';
+import { BATCH_ROLLBACK, BATCH_TOP_AGENTS, BATCH_TOP_PROPERTIES } from './lib/config';
 
 @Controller()
 export class BatchController {
-  constructor(private readonly nestarBatchService: BatchService) {}
+	private logger: Logger = new Logger('BatchController');
 
-  @Get()
-  getHello(): string {
-    return this.nestarBatchService.getHello();
-  }
+	constructor(private readonly batchService: BatchService) {}
+
+	/*
+	@Interval(1000)
+	hanleInterval() {
+		this.logger.debug('Interval test');
+	}
+
+
+	*/
+	@Timeout(1000)
+	hadnleTimeOut() {
+		this.logger.debug('Batch Server is ready');
+	}
+
+	@Cron('*/20 * * * * *', { name: 'CRON_TEST' })
+	public cronTest() {
+		this.logger['context'] = 'CRON_TEST';
+		this.logger.debug('EXECUTED');
+	}
+
+	@Cron('00 *  * * * *', { name: BATCH_ROLLBACK })
+	public async batchRollback() {
+		try {
+			this.logger['context'] = BATCH_ROLLBACK;
+			this.logger.debug('EXECUTED');
+			await this.batchService.batchRollback();
+		} catch (err) {
+			this.logger.error(err);
+		}
+	}
+
+	@Cron('20 *  * * * *', { name: BATCH_TOP_PROPERTIES })
+	public async batchProperties() {
+		try {
+			this.logger['context'] = BATCH_TOP_PROPERTIES;
+			this.logger.debug('EXECUTED');
+			await this.batchService.batchProperties();
+		} catch (err) {
+			this.logger.error(err);
+		}
+	}
+	@Cron('40 *  * * * *', { name: BATCH_TOP_AGENTS })
+	public async batchAgents() {
+		try {
+			this.logger['context'] = BATCH_TOP_AGENTS;
+			this.logger.debug('EXECUTED');
+			await this.batchService.batchAgents();
+		} catch (err) {
+			this.logger.error(err);
+		}
+	}
+	@Get()
+	getHello(): string {
+		return this.batchService.getHello();
+	}
 }
