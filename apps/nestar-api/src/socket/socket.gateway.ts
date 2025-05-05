4d2d090f2935bc2311f387613938e04e45e25016
mirzohidd <mirzohidmurodov77@gmail.com>
2025-05-05 13:18:40 +1000

    fix:modify WebSocket Chatting Server final stage

--- apps/nestar-api/src/socket/socket.gateway.ts
+++ apps/nestar-api/src/socket/socket.gateway.ts
@@ -2,51 +2,89 @@ import { Logger } from '@nestjs/common';
 import { OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { info } from 'console';
 import { Server, WebSocket } from 'ws';
+import { AuthService } from '../components/auth/auth.service';
+import { Member } from '../libs/dto/member/member';
 
+import * as url from 'url';
 interface MessagePayload {
 	event: string;
 	text: string;
+	memberData?: Member | null;
 }
 interface InfoPayload {
 	event: string;
 	totalClients: number;
+	memberData?: Member | null;
+	action: string;
 }
 
-@WebSocketGateway({ transports: ['websocket'], secure: false })
+@WebSocketGateway({ transports: ['websocket'], secundifinedre: false })
 export class SocketGateway implements OnGatewayInit {
 	private logger: Logger = new Logger('SocketEventsGateway');
 	private summaryClient: number = 0;
-
+	private clientAuthMap = new Map<WebSocket, Member | null>();
+	private messageList: MessagePayload[] = [];
+	constructor(private authService: AuthService) {}
 	@WebSocketServer()
 	server: Server;
 	public afterInit(server: any) {
 		this.logger.verbose(`WebSocket server Initialized & total [${this.summaryClient}]`);
 	}
-
-	handleConnection(client: WebSocket, ...args: any[]) {
+	private async retrieveAuth(req: any): Promise<Member | null> {
+		try {
+			const parseUrl = url.parse(req.url, true);
+			const { token } = parseUrl.query;
+			return await this.authService.verifyToken(token as string);
+		} catch (err) {
+			return null;
+		}
+	}
+	public async handleConnection(client: WebSocket, req: any) {
+		const authMember = await this.retrieveAuth(req);
+		console.log(authMember);
 		this.summaryClient++;
-		this.logger.verbose(` Connection & total [${this.summaryClient} ]`);
+		this.clientAuthMap.set(client, authMember);
+
+		const clientNick: string = authMember?.memberNick ?? 'Guest';
+		this.logger.verbose(` Connection [${clientNick}] & total [${this.summaryClient} ]`);
 
 		const infoMsg: InfoPayload = {
 			event: 'info',
 			totalClients: this.summaryClient,
+			memberData: authMember,
+			action: 'joined',
 		};
 		this.emitMessage(infoMsg);
+
+		client.send(JSON.stringify({ event: 'getMEssages', list: this.messageList }));
 	}
-	handleDisconnect(client: WebSocket) {
+	public handleDisconnect(client: WebSocket) {
+		const authMember = this.clientAuthMap.get(client);
 		this.summaryClient--;
-		this.logger.verbose(` Disconnection & total[${this.summaryClient}] `);
+		this.clientAuthMap.delete(client);
+		const clientNick: string = authMember?.memberNick ?? 'Guest';
+		this.logger.verbose(` Disconnection [${clientNick} & total[${this.summaryClient}] `);
 		const infoMsg: InfoPayload = {
 			event: 'info',
 			totalClients: this.summaryClient,
+			memberData: authMember,
+			action: 'left',
 		};
 
 		this.broadCastMessage(client, infoMsg);
 	}
 	@SubscribeMessage('message')
 	public async handleMessage(client: any, payload: any): Promise<void> {
-		const newMessage: MessagePayload = { event: 'message', text: payload };
-		this.logger.verbose(`NEW MESSAGE:${payload}`);
+		const authMember = this.clientAuthMap.get(client);
+		const newMessage: MessagePayload = { event: 'message', text: payload, memberData: authMember };
+		const clientNick: string = authMember?.memberNick ?? 'Guest';
+
+		this.logger.verbose(`NEW MESSAGE [${clientNick}] : ${payload}`);
+
+		this.messageList.push(newMessage);
+
+		if (this.messageList.length >= 5) this.messageList.splice(0, this.messageList.length - 5);
+
 		this.emitMessage(newMessage);
 	}
 	private broadCastMessage(sender: WebSocket, message: InfoPayload | MessagePayload) {
@@ -65,3 +103,10 @@ export class SocketGateway implements OnGatewayInit {
 		});
 	}
 }
+
+/*
+MESSAGE TARGT:
+1.CLIENT(only client)
+2.BROADCAST(except client)
+3.Emit(all clients)
+*/
